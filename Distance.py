9d23624fe28db4fc1353a8dee345f5a16eeaca1f
deborah-O <43342021+deborah-O@users.noreply.github.com>
2018-09-21 10:38:01 +0100

    function for comp minimum distance

--- Distance.py
+++ Distance.py
@@ -10,10 +10,37 @@ File for computing the minimum distance between pubs
 import math
 
 def comp_dist(point1, point2):
+    '''Compute the distance between two points, the points should be given as a 
+    tuple with x and y co-ords. The function returns the distance, which is 
+    computed with pythagoras.'''
+
     return math.sqrt((point1[0] - point2[0]) **2
                      + (point1[1] - point2[1]) **2)    
+
+
+def comp_minimum_distance(points):
+    '''Compute the minimum dsistance between several points. Givern the list 
+    of points, compute ditance between all pairs and return the minimum of 
+    these distances'''
     
+    result = None
+    for i in range(len(points)):
+        for j in range(len(points)):
+            if i == j:
+                continue
+            distance = comp_dist(points[i], points[j])
+            if result ==None:
+                result = distance
+            elif distance < result:
+                result = distance
+    return result
+
 
 point1 = (0,0)
 point2 = (1,1)
 print(comp_dist(point1, point2))
+print(comp_dist(point1, point1))
+
+point3 = (1,0)
+list_of_points = [point1, point2, point3]
+print(comp_minimum_distance(list_of_points))
