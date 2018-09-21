9da7ab62a192372baa019042ca36b95d28079601
deborah-O <43342021+deborah-O@users.noreply.github.com>
2018-09-21 10:05:34 +0100

    Define compute distance function

--- Distance.py
+++ Distance.py
@@ -4,6 +4,11 @@ Created on Fri Sep 21 09:58:09 2018
 
 @author: mm17do
 """
-'''
+'''Day 5
 File for computing the minimum distance between pubs
 '''
+import math
+
+def comp_dist(point1, point2):
+    return math.sqrt((point1[0] - point2[0]) **2
+                     + (point1[1] - point2[1]) **2)    
